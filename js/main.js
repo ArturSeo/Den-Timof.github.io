b7e2a098c1541e369efdcd8313710e35fb7fc179
Denis Timofeev <den.timofeev.1994@mail.ru>
2017-11-04 05:43:25 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(window).load(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        itemWidth: 565,
+        itemMargin: 15
+    });
+
+    $('.flexslider-drink').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        itemWidth: 330
+    });
+});
